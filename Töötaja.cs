ef141546e6bafcbcf124a97a76dec11ee50f1f00
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2025-09-19 11:25:30 +0300

    Create Töötaja.cs

---
+++ Töötaja.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace TARpv24_Ckeel
+{
+    public class Töötaja : Inimene
+    {
+        public string Ametikoht = "Keevitaja";
+        public double Tunnitasu = 15.50;
+        public int Tunnid { get; set; }
+
+        public void Töötan()
+        {
+            Console.WriteLine($"{Nimi} töötan ametikohal {Ametikoht}.");
+        }
+
+        public override void Mida_teeb()
+        {
+            Console.WriteLine($"{Nimi} töötab ametikohal.");
+        }
+
+        public double ArvutaPalk()
+        {
+            return Tunnitasu * Tunnid;
+        }
+    }
+}
