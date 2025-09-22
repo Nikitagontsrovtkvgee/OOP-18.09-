c55a8f107179849a89c1a369e058cd3159361e69
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2025-09-22 08:47:03 +0300

    Update ProgramSõidukid.cs

--- ProgramSõidukid.cs
+++ ProgramSõidukid.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace TARpv24_Ckeel
 {
@@ -9,9 +10,54 @@ namespace TARpv24_Ckeel
         {
             List<ISõiduk> sõidukid = new List<ISõiduk>();
 
+            // --- Failist lugemine (bonus) ---
+            if (File.Exists("andmed.txt"))
+            {
+                Console.WriteLine("Loeme sõidukid failist andmed.txt ...");
+                string[] read = File.ReadAllLines("andmed.txt");
+
+                foreach (string rida in read)
+                {
+                    try
+                    {
+                        string[] osad = rida.Split(';');
+                        switch (osad[0])
+                        {
+                            case "Auto":
+                                sõidukid.Add(new Auto(
+                                    double.Parse(osad[1]),
+                                    double.Parse(osad[2]),
+                                    double.Parse(osad[3])));
+                                break;
+                            case "Jalgratas":
+                                sõidukid.Add(new Jalgratas(double.Parse(osad[1])));
+                                break;
+                            case "Buss":
+                                sõidukid.Add(new Buss(
+                                    double.Parse(osad[1]),
+                                    double.Parse(osad[2]),
+                                    double.Parse(osad[3]),
+                                    int.Parse(osad[4])));
+                                break;
+                            case "Elektritõukeratas":
+                                sõidukid.Add(new Elektritõukeratas(
+                                    double.Parse(osad[1]),
+                                    double.Parse(osad[2]),
+                                    double.Parse(osad[3])));
+                                break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Viga failist lugemisel: {ex.Message}");
+                    }
+                }
+            }
+
+            // --- Kasutaja sisestamine ---
             while (true)
             {
-                Console.WriteLine("\nVali sõiduk: 1=Auto, 2=Jalgratas, 3=Buss, 0=Välju");
+                Console.WriteLine("\nVali sõiduk: 1=Auto, 2=Jalgratas, 3=Buss, 4=Elektritõukeratas, 0=Välju");
                 string valik = Console.ReadLine();
                 if (valik == "0") break;
 
@@ -32,7 +78,7 @@ namespace TARpv24_Ckeel
                         case "2":
                             Console.Write("Läbitud km: ");
                             double kmBike = double.Parse(Console.ReadLine());
-                            sõидukid.Add(new Jalgratas(kmBike));
+                            sõidukid.Add(new Jalgratas(kmBike));
                             break;
 
                         case "3":
@@ -47,6 +93,16 @@ namespace TARpv24_Ckeel
                             sõidukid.Add(new Buss(kuluB, kmB, hindB, reisijad));
                             break;
 
+                        case "4":
+                            Console.Write("Aku kulu (kWh/100km): ");
+                            double aku = double.Parse(Console.ReadLine());
+                            Console.Write("Läbitud km: ");
+                            double kmE = double.Parse(Console.ReadLine());
+                            Console.Write("Elektri hind €/kWh: ");
+                            double elekter = double.Parse(Console.ReadLine());
+                            sõidukid.Add(new Elektritõukeratas(aku, kmE, elekter));
+                            break;
+
                         default:
                             Console.WriteLine("Tundmatu valik!");
                             break;
@@ -58,6 +114,7 @@ namespace TARpv24_Ckeel
                 }
             }
 
+            // --- Tulemused ---
             Console.WriteLine("\n--- Sõidukite tulemused ---");
             double koguKulu = 0;
             foreach (var s in sõidukid)
