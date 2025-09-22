0a3a8af537d132ced808ed6eff0a07553e0593e4
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2025-09-22 08:45:25 +0300

    Update Jalgratas.cs

--- Jalgratas.cs
+++ Jalgratas.cs
@@ -2,33 +2,22 @@ using System;
 
 namespace TARpv24_Ckeel
 {
-    public class Buss : ISõiduk
+    public class Jalgratas : ISõiduk
     {
-        public double Kütusekulu100km { get; set; }
         public double LäbitudKm { get; set; }
-        public double KütuseHind { get; set; }
-        public int ReisijateArv { get; set; }
 
-        public Buss(double kütusekulu100km, double läbitudKm, double kütuseHind, int reisijateArv)
+        public Jalgratas(double läbitudKm)
         {
-            Kütusekulu100km = kütusekulu100km;
             LäbitudKm = läbitudKm;
-            KütuseHind = kütuseHind;
-            ReisijateArv = reisijateArv;
         }
 
-        public double ArvutaKulu()
-        {
-            double liitrid = (Kütusekulu100km / 100.0) * LäbitudKm;
-            double koguKulu = liitrid * KütuseHind;
-            return koguKulu / ReisijateArv; // jagame reisijate vahel
-        }
+        public double ArvutaKulu() => 0; // Jalgratas ei kasuta kütust
 
         public double ArvutaVahemaa() => LäbitudKm;
 
         public override string ToString()
         {
-            return $"Buss: {LäbitudKm} km, kulu per reisija: {ArvutaKulu():F2} €";
+            return $"Jalgratas: {LäbitudKm} km, kulu: {ArvutaKulu():F2} €";
         }
     }
 }
